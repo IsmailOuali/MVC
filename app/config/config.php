56f4e8fd1298d1b38adaa63e8b93eb2258ceaa75
unknown <ismail.ouali@ump.ac.ma>
2024-01-05 12:16:14 +0100

    adding the connection to database

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'paroly');
+
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://localhost/PAROLY');
+
+define('SITENAME', 'PAROLY');
